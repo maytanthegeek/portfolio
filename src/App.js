2e17584f20471f068a5c021ccdc0db2c27e2fb6e
Tanmay <maytan.thegeek@gmail.com>
2018-04-01 23:08:03 +0530

    Started with material-ui.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+
+import { AppBar } from 'material-ui';
+import Home from './Home';
+import { MuiThemeProvider } from 'material-ui/styles';
+import muiTheme from './theme'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <AppBar title="My Home"></AppBar>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            {/* <Route exact path="/about" component={About}></Route> */}
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import './index.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
+
+injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
