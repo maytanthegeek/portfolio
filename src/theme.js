ad36da59b281a21956592a62904ea45d44f1b69d
Tanmay <maytan.thegeek@gmail.com>
2018-04-01 23:08:52 +0530

    Started with material-ui.

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { grey500 } from 'material-ui/styles/colors';
+import { grey700 } from 'material-ui/styles/colors';
+import { redA700 } from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: grey500,
+    primary2Color: grey700,
+    accent1Color: redA700,
+    pickerHeaderColor: grey500
+  }
+});
+
+export default muiTheme;
