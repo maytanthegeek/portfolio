98a030691af3a903fd746c0ce3eb3d2004f8dfec
Tanmay <maytan.thegeek@gmail.com>
2020-09-06 21:51:15 +0530

    Updates

---
+++ precache-manifest.2debf95d78e7c39a96512f8d410b1c8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b122c287ae3f26d38104743063d0c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b52d69e9686c1e697f6e",
+    "url": "/portfolio/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "867a87f7b48f5c1bba06",
+    "url": "/portfolio/static/css/main.40dfbc16.chunk.css"
+  },
+  {
+    "revision": "b52d69e9686c1e697f6e",
+    "url": "/portfolio/static/js/2.fa73360f.chunk.js"
+  },
+  {
+    "revision": "03de776f7506836116c32238de3168c2",
+    "url": "/portfolio/static/js/2.fa73360f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867a87f7b48f5c1bba06",
+    "url": "/portfolio/static/js/main.c2a82b6e.chunk.js"
+  },
+  {
+    "revision": "1fa4c20a0e41bbe88db1",
+    "url": "/portfolio/static/js/runtime-main.3a9c6e80.js"
+  },
+  {
+    "revision": "556527f8a603b9c7a424476ba3893200",
+    "url": "/portfolio/static/media/me.556527f8.jpg"
+  }
+]);
